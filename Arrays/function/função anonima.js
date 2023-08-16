0b9eae64b48043e3c7863af760e2623b60539558
Samuel Douglas Martins Marques <samuel30douglas@gmail.com>
2023-08-16 18:04:04 -0300

    função anonimas

---
+++ Arrays/function/função anonima.js
@@ -0,0 +1,25 @@
+/*let exibirIdade = function (idade){
+    console.log(idade);
+}*/
+
+/*function calcular(text) {
+    console.log (text)
+}*/
+const exibir = function(text){
+    console.log(text)
+}
+
+exibir("treinando função anonima");
+
+const somar = () => {
+    console.log (2 +4);
+}
+
+// diferença de função e de função anoninma
+const seuNome = function(){
+    let nome = prompt("digite seu nome")
+}
+
+function colocarNome(){
+    console.log("seu nome")
+}
